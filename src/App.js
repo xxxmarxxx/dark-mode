fa795e0c8734e01bc149dc79f867ab28d171a103
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-10 12:19:52 +0100

    add main section div button data.map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from "react";
+import data from "./data";
+import Article from "./Article";
+// lesson 204/
 
 function App() {
-  return <h2>dark mode starter</h2>
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn">toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
