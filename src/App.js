2ad98541682c43f7c1e69aaab2576a9ddfd78961
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-10 13:40:49 +0100

    add const toggletheme useEffect

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Article from "./Article";
 // lesson 204/205/
 
 function App() {
+  const [theme, setTheme] = useState("light-theme");
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
